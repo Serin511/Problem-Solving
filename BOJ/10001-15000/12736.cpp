38e71458d94b6fc8851b69557b1d84d6786fc4b8
dsstar <dennisstar2003@gmail.com>
2020-02-27 03:03:49 +0900

    200227

--- BOJ/10001-15000/12736.cpp
+++ BOJ/10001-15000/12736.cpp
@@ -1,18 +1,9 @@
 #include <bits/stdc++.h>
-#define fi first
-#define se second
-#define ryan bear
-#define sq(X) ((X)*(X))
 #define em emplace
 #define eb emplace_back
-#define all(V) (V).begin(), (V).end()
-#define unq(V) (V).erase(unique(all(V)), (V).end())
 using namespace std;
 typedef long long ll;
-typedef vector<ll> vlm;
 typedef vector<int> vim;
-typedef pair<ll, ll> pll;
-typedef pair<int, int> pii;
 
 int N, M;
 vim adj[300010]; ll C[300010], D[300010];
@@ -20,36 +11,25 @@ priority_queue<ll> P[300010];
 
 void Merge(int x, int y) {
 	if (P[x].size()<P[y].size()) swap(P[x], P[y]);
-	while (!P[y].empty()) { P[x].push(P[y].top()); P[y].pop(); }
+	while (P[y].size()) P[x].em(P[y].top()), P[y].pop();
 }
 
-void dfs(int now) {
-	D[now]=C[now];
-	if (now>N) {
-		P[now].em(C[now]); P[now].em(C[now]);
-		return ;
-	}
-	for (auto &i:adj[now]) {
-		dfs(i); D[now]+=D[i];
-		Merge(now, i);
-	}
-	for (int i=1; i<adj[now].size(); i++) P[now].pop();
-	ll x = P[now].top(); P[now].pop();
-	ll y = P[now].top(); P[now].pop();
-	P[now].em(x+C[now]); P[now].em(y+C[now]);
+void dfs(int n) {
+	D[n]=C[n];
+	if (n>N) { P[n].em(C[n]), P[n].em(C[n]); return ; }
+	for (auto &i:adj[n]) dfs(i), D[n]+=D[i], Merge(n, i);
+	for (int i=1; i<adj[n].size(); i++) P[n].pop();
+	ll x=P[n].top(); P[n].pop();
+	ll y=P[n].top(); P[n].pop();
+	P[n].em(x+C[n]); P[n].em(y+C[n]);
 }
 
 int main() {
-	scanf("%d %d", &N, &M);
-	for (int i=2; i<=N+M; i++) {
-		int P; scanf("%d %lld", &P, &C[i]);
-		adj[P].eb(i);
-	}
+	cin>>N>>M;
+	for (int i=2, p; i<=N+M; i++) cin>>p>>C[i], adj[p].eb(i);
 	dfs(1); P[1].em(0);
-	ll ans=D[1]; ll x=P[1].top(); P[1].pop();
-	for (int i=0; !P[1].empty(); P[1].pop(), i++) {
-		ans-=i*(x-P[1].top()); x=P[1].top();
-	}
-	printf("%lld\n", ans);
+	ll A=D[1]; ll x=P[1].top(); P[1].pop(); int i=0;
+	while (P[1].size()) A-=i*(x-P[1].top()), x=P[1].top(), P[1].pop(), i++;
+	cout<<A<<'\n';
 	return 0;
 }
